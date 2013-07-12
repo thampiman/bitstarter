5f1951f7e039ada295227a656fbed56ad12b49ef
Ajay Thampi <ajay.thampi@gmail.com>
2013-07-12 18:49:04 +0000

    reading data from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var data = fs.readFileSync(this.dir + "/index.html");
+  
+  response.send(data.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
